a7dc5afbee5391b6c42d626ecaa41291cf4d6933
reisie <52404357+reisie@users.noreply.github.com>
2019-07-31 11:23:54 +0200

    disable import-feature
    Not working reliable (at all?)

--- client/src/components/dashboard/DActions/DActions.vue
+++ client/src/components/dashboard/DActions/DActions.vue
@@ -5,29 +5,23 @@
         <v-toolbar-title class="subheading">Actions</v-toolbar-title>
       </v-toolbar>
       <v-card-actions>
-        <v-btn primary @click.stop="showDActionsImportDialog()"><v-icon dark left>fa-upload</v-icon>Import</v-btn>
         <v-btn primary @click.stop="showDActionsExportDialog()"><v-icon dark left>fa-download</v-icon>Export</v-btn>
       </v-card-actions>
     </v-card>
 
     <d-actions-export-dialog ref="dActionsExportDialog" class="pa-0 ma-0"></d-actions-export-dialog>
-    <d-actions-import-dialog ref="dActionsImportDialog" class="pa-0 ma-0"></d-actions-import-dialog>
   </div>
 </template>
 
 <script>
   import rdflibService from '@/rdflib-services/rdflib-service'
   import DActionsExportDialog from '@/components/dashboard/DActions/dialogs/DActionsExportDialog'
-  import DActionsImportDialog from '@/components/dashboard/DActions/dialogs/DActionsImportDialog'
   export default {
     name: 'DActions',
-    components: {DActionsExportDialog, DActionsImportDialog},
+    components: {DActionsExportDialog},
     methods: {
       showDActionsExportDialog () {
         this.$refs.dActionsExportDialog.show()
-      },
-      showDActionsImportDialog () {
-        this.$refs.dActionsImportDialog.show()
       }
     },
     mounted () {
